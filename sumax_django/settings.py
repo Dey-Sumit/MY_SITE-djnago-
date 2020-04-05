154cae28e0b9ff508b710e50031c38b39dec53ff
unknown <sumax333@gmail.com>
2020-04-05 18:14:11 +0530

    blog page added

--- sumax_django/settings.py
+++ sumax_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uk=z95=i3@+5g+pwcsolzocbg*udmrhq2&033e^!4x_0ojo91='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
